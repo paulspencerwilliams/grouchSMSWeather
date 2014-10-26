3c728c4190e3bf5193578053a131a1ae9b1b49ba
Paul Williams <paul@paulswilliams.me.uk>
2014-10-26 08:23:18 +0000

    refactored cucumber feature

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,8 @@
-Then(/^a text message with following content should be sent:$/) do |expected_body|
+Then(/^a text message should be sent to "(.*?)" stating "(.*?)"$/) do | expected_phone_number, expected_message_text|
   expect(WebMock).to have_requested(:post, 'https://api.clockworksms.com/xml/send').with { | request | 
-    request.body.include?('<Content>It is 62.7 degrees and Mostly Cloudy</Content>') && request.body.include?('<To>07590389430</To>')}
+    expected_to = "<To>#{expected_phone_number}</To>"
+    expected_content = "<Content>#{expected_message_text}</Content>"
 
+    request.body.include?(expected_content) && request.body.include?(expected_to)}
 end
+
