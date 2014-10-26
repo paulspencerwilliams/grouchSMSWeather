2d2f0d0f04ccd9f6bcd4508d0ea9b03064cc3214
Paul Williams <paul@paulswilliams.me.uk>
2014-10-26 01:55:06 +0000

    now recording interactions with both services

--- features/sms_weather_to_users.feature
+++ features/sms_weather_to_users.feature
@@ -1,5 +1,6 @@
 Feature: as a user interested in receiving nonsense text messages, when a cron job kick in, I will be notified of the weather on the other side of the globe!
 
+  @vcr
   Scenario: Happy path
     When I run `txt-weather`
     Then the output should contain:
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,40 @@
 require 'aruba/cucumber'
+require 'aruba'
+require 'aruba/in_process'
+require 'vcr'
+require 'webmock'
+require 'grouchSMSWeather'
 
+VCR.cucumber_tags do |t|
+  t.tag  '@vcr', :use_scenario_name => true
+end
+
+VCR.configure do |c|
+  c.hook_into :webmock
+  c.cassette_library_dir     = 'features/cassettes'
+  c.default_cassette_options = { :record => :new_episodes }
+end
+
+class VcrFriendlyMain
+  def initialize(argv, stdin, stdout, stderr, kernel)
+    @argv, @stdin, @stdout, @stderr, @kernel = argv, stdin, stdout, stderr, kernel
+  end
+
+  def execute!
+    $stdin = @stdin
+    $stdout = @stdout
+    GrouchSMSWeather::CLI.start(@argv)
+  end
+end
+
+Before('@vcr') do
+  Aruba::InProcess.main_class = VcrFriendlyMain
+  Aruba.process = Aruba::InProcess
+end
+
+After('@vcr') do
+  Aruba.process = Aruba::SpawnProcess
+  VCR.eject_cassette
+  $stdin = STDIN
+  $stdout = STDOUT
+end
